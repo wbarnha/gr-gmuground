f4ef40e2544b2f61ff83bb3c2fa5a59d5a239e88
wbarnha <williambbarnhart@gmail.com>
2020-01-26 17:01:33 -0500

    Removed erroneously included files and added snr

--- build/docs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wbarnhart/gr-gmuground/docs
-# Build directory: /home/wbarnhart/gr-gmuground/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doxygen")
--- build/python/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wbarnhart/gr-gmuground/python
-# Build directory: /home/wbarnhart/gr-gmuground/build/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(qa_sqrt "/bin/sh" "/home/wbarnhart/gr-gmuground/build/python/qa_sqrt_test.sh")
-add_test(qa_snr_selector "/bin/sh" "/home/wbarnhart/gr-gmuground/build/python/qa_snr_selector_test.sh")
