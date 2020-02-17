50180571bc0bdb5d0e42cd96f84502b018f1442a
wbarnha <williambbarnhart@gmail.com>
2020-02-16 20:01:51 -0500

    Added new testing blocks in examples

--- build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wbarnhart/gr-gmuground/lib
-# Build directory: /home/wbarnhart/gr-gmuground/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
