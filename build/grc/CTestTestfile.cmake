16c5caa74f37a898a0869545a5aa30d27ac097ce
wbarnha <williambbarnhart@gmail.com>
2020-02-24 15:58:39 -0500

    Cleanup

--- build/grc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wbarnhart/gr-gmuground/grc
-# Build directory: /home/wbarnhart/gr-gmuground/build/grc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
